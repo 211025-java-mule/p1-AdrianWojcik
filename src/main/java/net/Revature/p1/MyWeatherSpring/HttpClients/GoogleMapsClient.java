45ba67fa8b8c244103fe7072288bd60a983d9115
Adrian Wójcik <71228795+eAdrian89@users.noreply.github.com>
2021-11-27 20:01:27 +0100

    Update GoogleMapsClient.java

--- src/main/java/net/Revature/p1/MyWeatherSpring/HttpClients/GoogleMapsClient.java
+++ src/main/java/net/Revature/p1/MyWeatherSpring/HttpClients/GoogleMapsClient.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class GoogleMapsClient {
 
-    private final String apiKey = "AIzaSyBJ-vazF391IBggyLT3gNZwF9336oZuc-c";
+    private final String apiKey = "";
 
     public String getMap(String city){
         return "https://maps.googleapis.com/maps/api/staticmap?center=" + city + "&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key="+ apiKey;
